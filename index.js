8382f2dbccba5d8a60c6b2444db02bf57e7a1d8e
Mary <ipadlover8322@gmail.com>
2018-05-12 14:51:29 -0400

    replace needs the second arg

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on("messageReactionAdd", function(reaction, user) {
   if (user.id != MARY_ID) return;
   if (reaction.message.channel.id != CHANNEL_ID) return;
   if (!reaction.message.content.startsWith("r!tts ")) return;
-  const content = reaction.message.content.replace("r!tts ");
+  const content = reaction.message.content.replace("r!tts ", "");
   fs.writeFile("/data/data/com.termux/files/home/.tts", content, {
     encoding: "utf-8"
   });
